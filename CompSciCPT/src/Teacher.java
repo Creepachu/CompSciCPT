aa4effbe74c36cf66615a93cd98e9f0278645d80
Creepachu <44036564+Creepachu@users.noreply.github.com>
2018-12-19 11:23:58 -0500

    Update Teacher.java

--- CompSciCPT/src/Teacher.java
+++ CompSciCPT/src/Teacher.java
@@ -1,37 +1,38 @@
 class Teacher {
 
-Boolean isLunchSupervisor = false; 
-Boolean isAbsent = false;
-String name;
-int periodOff;
-String lunch;
- 
- Teacher(String n, int p, String l)//Constructor
- {
-   name = n;
-   periodOff = p;
-   lunch = l;
-   if (p == 3) isLunchSupervisor = true;
- }
- 
- String getName()
- {
-   return name;
- }
- 
- int getPeriodOff()
- {
-  return periodOff;
- }
- 
- String getLunch()
- {
-   return lunch;
- }
- 
- Boolean getAbsent()
- {
-   return isAbsent;
- }
- 
-}//End of teacher
+    Boolean isLunchSupervisor = false;
+    Boolean isAbsent = false;
+
+    String name;
+    int periodOff;
+    String lunchPeriod;
+    Teacher(String n)//Constructor
+    {
+        name = n;
+    }
+
+    String getName()
+    {
+        return name;
+    }
+
+    int getPeriodOff()
+    {
+        return periodOff;
+    }
+
+    Boolean getAbsent()
+    {
+        return isAbsent;
+    }
+
+    void setPeriodOff(int period)
+    {
+        periodOff = period;
+    }
+
+    String getLunch()
+    {
+        return lunchPeriod;
+    }
+}
