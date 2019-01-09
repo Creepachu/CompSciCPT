2163fafd554fcb399ee50fe1d6be1eef9d929660
Creepachu <44036564+Creepachu@users.noreply.github.com>
2019-01-09 11:28:48 -0500

    Update TeacherStuffMain.java

--- CompSciCPT/src/TeacherStuffMain.java
+++ CompSciCPT/src/TeacherStuffMain.java
@@ -12,13 +12,31 @@ public class TeacherStuffMain {
             teacherScanner = new Scanner(teacherFile);
         } catch (FileNotFoundException e) {
         }
+        boolean input = initialize();
 
-        generateTeachers();
-        makeSomeoneAbsent();
-        printNames();
-        checkAbsent();
+        while (input){
+            generateTeachers();
+            makeSomeoneAbsent();
+            printNames();
+            checkAbsent();
+        }
     }
 
+    static boolean initialize() {
+            System.out.println("A new day! Run program?");
+            System.out.println();
+            boolean runProgram = TextIO.getBoolean();
+
+            if (runProgram) return runProgram;
+
+            if (!runProgram) {
+                System.out.println("This will close the program and restart the semester. Continue?");
+                boolean yesImSure = TextIO.getBoolean();
+                if (yesImSure) return runProgram;
+                else if (!yesImSure) initialize();
+            }
+            return false;
+    }
     static void generateTeachers() {
         String currentLine;
         int i = 0;
@@ -34,38 +52,74 @@ public class TeacherStuffMain {
                     teachers[i].isLunchSupervisor = true;
                     break;
                 }
-                if (Integer.parseInt(value) == 0) teachers[i].periodOff = (j+1);
+                if (Integer.parseInt(value) == 0) teachers[i].periodOff = (j + 1);
             }
             i++;
         }
     }
 
-     static void checkAbsent() {
-      Teacher[] temp = new Teacher[1];
-      int a = 0;
-      for (Teacher teacher: teachers) {
-        if (teacher.getAbsent()) {
-          System.out.println();
-          System.out.println(teacher.getName() + " is absent");
-          temp[0] = teacher;
-          System.out.println(temp[0].getName() + " has period " + temp[0].getPeriodOff() + " off ");
-          System.out.println();
-          
-          for (int j = 0; j < teachers.length; j++) {
-            if (teachers[j].getPeriodOff() != temp[0].getPeriodOff()) {
-              //add suggestions for replacements
-              System.out.println(teachers[j].getName() + " can replace " + temp[0].getName() + " for period " + teachers[j].getPeriodOff());
-              a++;
-              if(a == 3)
-              {
-                break;
-              }
+    static void checkAbsent() {
+        Teacher temp;
+
+        for (Teacher teacher : teachers) {
+            if (teacher.getAbsent()) {
+                System.out.println();
+                System.out.println(teacher.getName() + " is absent");
+                System.out.println();
+                temp = teacher;
+
+                findReplacements(temp);
+                //check for booleans
             }
-          }
-          //check for booleans
         }
-      }
     }//End of checkAbsent
+
+    static void findReplacements(Teacher absentee) {
+
+        if (absentee.periodOff != 1){
+            int p1fill = 0;
+            for (int i = 0; i < teachers.length; i++) {
+                if (p1fill == 2) break;
+                if (teachers[i].periodOff == 1){
+                    System.out.println(teachers[i].getName() + " can replace " + absentee.getName() + " for period 1.");
+                    p1fill++;
+                }
+            }
+        }
+
+        if (absentee.periodOff != 2){
+            int p2fill = 0;
+            for (int i = 0; i < teachers.length; i++) {
+                if (p2fill == 2) break;
+                if (teachers[i].periodOff == 2){
+                    System.out.println(teachers[i].getName() + " can replace " + absentee.getName() + " for period 2.");
+                    p2fill++;
+                }
+            }
+        }
+
+        if (absentee.periodOff != 3){
+            int p3fill = 0;
+            for (int i = 0; i < teachers.length; i++) {
+                if (p3fill == 2) break;
+                if (teachers[i].periodOff == 3){
+                    System.out.println(teachers[i].getName() + " can replace " + absentee.getName() + " for period 3.");
+                    p3fill++;
+                }
+            }
+        }
+
+        if (absentee.periodOff != 4){
+            int p4fill = 0;
+            for (int i = 0; i < teachers.length; i++) {
+                if (p4fill == 2) break;
+                if (teachers[i].periodOff == 4){
+                    System.out.println(teachers[i].getName() + " can replace " + absentee.getName() + " for period 4.");
+                    p4fill++;
+                }
+            }
+        }
+    }
 
     static void printNames() {
         for (Teacher teacher : teachers) {
@@ -91,7 +145,8 @@ public class TeacherStuffMain {
             }
         } while (!teacherFound);
         //teachers[randomWithRange(0,teachers.length)].isAbsent = true;
-    } // end of makesomeoneabsent
+    }
+
     static int randomWithRange(int min, int max) {
         int range = (max - min) + 1;
         return (int) (Math.random() * range) + min;
