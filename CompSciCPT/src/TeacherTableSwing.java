f01badb71ad85c5a2904763c71dce60ed345b5ed
Creepachu <44036564+Creepachu@users.noreply.github.com>
2019-01-18 12:00:00 -0500

    Update TeacherTableSwing.java

--- CompSciCPT/src/TeacherTableSwing.java
+++ CompSciCPT/src/TeacherTableSwing.java
@@ -2,18 +2,17 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TeacherTableSwing extends JPanel {
+    static JTable table;
 
     public TeacherTableSwing(Teacher[] teachers) { // Constructor
         super(new GridLayout(1, 0));
         String[] columnNames = {"Teacher Name", "Period 1", "Period 2", "Period 3", "Period 4"
         };
         Object[][] data = new Object[4][3];
-
-
-        final JTable table = new JTable(data, columnNames);
         table.setPreferredScrollableViewportSize(new Dimension(900, 600));
         table.setFillsViewportHeight(true);
 
+        table = new JTable(data, columnNames);
         JScrollPane scrollPane = new JScrollPane(table);
         add(scrollPane);
         showTable();
@@ -22,9 +21,8 @@ public class TeacherTableSwing extends JPanel {
     private static void showTable() {
         JFrame frame = new JFrame("Teacher Schedule");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        TeacherTableSwing newContentPane = new TeacherTableSwing();
-        newContentPane.setOpaque(true);
-        frame.setContentPane(newContentPane);
+        table.setOpaque(true);
+        frame.setContentPane(table);
         frame.pack();
         frame.setVisible(true);
     }
